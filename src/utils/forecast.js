2746ced64fbcc0d207fae680c1179470a6a8168a
raveeee0 <80584055+raveeee0@users.noreply.github.com>
2022-05-24 19:59:10 +0200

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=c1c6dcc59fda6a1b954caa6d6b35ac40&query=' + latitude + ',' + longitude + '&units=m';
+    const url = 'http://api.weatherstack.com/current?access_key=xxxxxxxxxxxxxxxxxxxxxxxxx&query=' + latitude + ',' + longitude + '&units=m';
     request({ url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather services', undefined);

7c0597378bbf30c8f48b59c8367f0fa8e8421ffe
raveeee0 <80584055+raveeee0@users.noreply.github.com>
2022-05-24 19:58:54 +0200

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZ3JhdmVnZ2kiLCJhIjoiY2tyeXY0dWJ3MTN3ZjJ2bnEwYm9vbHRtbCJ9._iTvP8X1ryf0DahAayNDJA&limit=1&language=it';
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx&limit=1&language=it';
     request({ url, json: true }, (error, {body} = {}) => {
         if (error) {
             callback('Unable to connect to location services.', undefined);
